e62d045be0d5a7783c4f83cf35152393a8ce59d6
skylinebreaker <bwshen2016@gmail.com>
2018-09-06 00:24:12 -0700

    upload user profile image as well as username to the FireBase.

--- Instagram/ViewController.swift
+++ Instagram/ViewController.swift
@@ -9,14 +9,45 @@
 import UIKit
 import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     let plusPhotoButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(handlePlusPhoto), for: .touchUpInside)
         return button
     }()
+    
+    @objc func handlePlusPhoto() {
+        let imagePickerController = UIImagePickerController()
+        
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let editedImage = info["UIImagePickerControllerEditedImage"] as? UIImage {
+            
+            plusPhotoButton.setImage(editedImage.withRenderingMode(.alwaysOriginal), for: .normal)
+            
+        } else if let originalImage = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            
+            plusPhotoButton.setImage(originalImage.withRenderingMode(.alwaysOriginal), for: .normal)
+            
+        }
+        
+        plusPhotoButton.layer.cornerRadius = plusPhotoButton.frame.width / 2
+        plusPhotoButton.layer.masksToBounds = true
+        plusPhotoButton.layer.borderColor = UIColor.black.cgColor
+        plusPhotoButton.layer.borderWidth = 3
+        
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
     
     let emailTextField: UITextField = {
         let tf = UITextField()
@@ -99,6 +130,55 @@ class ViewController: UIViewController {
             }
             
             print("successfully create user:", user?.uid ?? "")
+            
+            guard let image = self.plusPhotoButton.imageView?.image else { return }
+            
+            guard let uploadData = UIImageJPEGRepresentation(image, 0.3) else { return }
+            
+            let filename = NSUUID().uuidString
+            let storageItem = Storage.storage().reference().child("profile_images").child(filename)
+            storageItem.putData(uploadData, metadata: nil, completion: { (metadata, err) in
+                
+                if let err = err {
+                    print("Failed to upload profile image", err)
+                    return
+                }
+                
+                storageItem.downloadURL(completion: {(url, error) in
+                    if let error = error {
+                        print(error)
+                        return
+                    }
+                    
+                    if let url = url {
+                        print("Successfully upload profile image", url)
+                        
+                        guard let uid = user?.uid else { return }
+                        
+                        let dictionaryValues = ["username": username, "profileImageUrl": url.absoluteString]
+                        let values = [uid: dictionaryValues]
+                        
+                        Database.database().reference().child("users").updateChildValues(values, withCompletionBlock: { (err, ref) in
+                            
+                            if let err = err {
+                                print("Fail to save user info into database", err)
+                                return
+                            }
+                            
+                            print("Successfully saved user info to db")
+                            
+                        })
+                    }
+                    
+                    
+                
+                })
+                
+            })
+            
+            
+            
+            
             
         }
         
